ac555a29378dbefbd81573a3ca3089a2db0e9e26
lucas schmitz <lucas.schmitz@institutoivoti.com.br>
2017-10-17 21:43:24 -0200

    criação de nova classe

---
+++ Grovy.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace console_dotnet
+{
+    class Grovy
+    {
+            public int calc(int a, int b, int c) {
+                return a / b * c;
+            }
+            
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,24 @@ namespace console_dotnet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Opaaaaaaa");
+            String nome;
+            nome = "Grovy";
+            // Console.WriteLine("Opaaaaaaa");
+            Console.WriteLine(nome);
+
+            Double valor = 2.2;
+            Double quantidade = 6.888;
+            Double resultado = valor * quantidade;
+            Console.WriteLine(resultado);
+
+            Boolean maiorQueDez = (resultado > 10);
+            if (maiorQueDez) {
+                System.Console.WriteLine($"{resultado} é maior que dez");
+            } else {
+                System.Console.WriteLine($"{resultado} é menor que dez");
+            }
+                Grovy a = new Grovy();
+                System.Console.WriteLine(a.calc(6, 2, 3));
         }
     }
 }
